52e70ae3843a8d5e57cea400fb02828982df5d92
nurbol-dev <baevnurbol@gmail.com>
2023-07-24 15:26:37 +0600

    fs

--- src/components/header/Header.tsx
+++ src/components/header/Header.tsx
@@ -1,10 +1,34 @@
 import React from 'react';
 import './header.scss'
+import logo from './../../assets/img/logo.svg'
+import {BsBell, BsBoxSeam} from "react-icons/bs";
+import {SlBasket} from "react-icons/sl";
+import {AiOutlineGlobal, AiOutlineHeart} from "react-icons/ai";
+import {LiaEnvelopeSolid} from "react-icons/lia";
+import {GoPerson} from "react-icons/go";
 const Header = () => {
     return (
-        <div>
+        <>
+            <div id='header'>
+                <div className="container">
+                    <div className="header">
+                        <img src={logo} alt=""/>
+                        <div className="header-nav">
+                            <BsBoxSeam/>
+                            <BsBell/>
+                            <SlBasket/>
+                        </div>
+                        <div className="header-nav">
+                            <AiOutlineGlobal/>
+                            <LiaEnvelopeSolid/>
+                            <AiOutlineHeart/>
+                            <GoPerson/>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </>
 
-        </div>
     );
 };
 
