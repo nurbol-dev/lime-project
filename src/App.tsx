56023f0db73f4faad3540ed8d31509d8ad0b4bb9
belekov <116719835+belekobdev@users.noreply.github.com>
2023-07-24 15:27:20 +0600

    starting (#1)

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,13 @@
 import React from 'react';
 import './App.scss';
 import Header from "./components/header/Header";
+import Authorization from './components/authorization/Authorization';
 
 function App() {
   return (
     <div className="App">
       <Header/>
+      <Authorization/>
     </div>
   );
 }
