769b00de0b1034977e2c25ce4e9869974028f9f0
belekov <116719835+belekobdev@users.noreply.github.com>
2023-07-31 19:57:49 +0600

    mkjb (#17)

--- src/index.tsx
+++ src/index.tsx
@@ -1,11 +1,15 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import {Provider} from "react-redux";
-import {setUpStore} from "./store";
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import { Route, Routes } from "react-router-dom";
+import App from "./App";
+import { Provider } from "react-redux";
+import { setUpStore } from "./store";
 import "slick-carousel/slick/slick.css";
 import "slick-carousel/slick/slick-theme.css";
+import { BrowserRouter } from "react-router-dom";
+import Products from "./components/Products/Products";
+
 import {BrowserRouter} from "react-router-dom";
 import Layout from "./layout";
 const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement)
@@ -18,4 +22,3 @@ root.render(
        </BrowserRouter>
     </Provider>
 );
-
---
+++ src/prob/prob.jsx
@@ -0,0 +1,10 @@
+const Prob = () => {
+  return (
+    <>
+      Lorem ipsum dolor sit amet consectetur adipisicing elit. Rerum voluptates
+      quo perspiciatis consequatur asperiores consectetur in quae! Ea,
+      temporibus quas!
+    </>
+  );
+};
+export default Prob;
